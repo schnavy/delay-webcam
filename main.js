e60ff2465a73da525b32175a375b39ba585dd165
David <50714711+schnavy@users.noreply.github.com>
2021-10-18 19:19:40 +0200

    minor

--- main.js
+++ main.js
@@ -4,32 +4,31 @@ let i = 0;
 let delay = 0;
 let slider = document.getElementById("delaySlider");
 let DelayDisp = document.getElementById("delayDisplay");
-let startZeit = new Date()
 
 function setup() {
 	createCanvas(640, 480);
     frameRate(25)
 	capture = createCapture(VIDEO);
-    fullscreen()
-
 }
 function draw() {
-	background(200);
 	if (capture.loadedmetadata) {
-		delay = int(slider.value);
+        console.log(capture);
+		delay = int(slider.value) + 1;
 
         DelayDisp.textContent = int(slider.value)/25 + " Sekunden"
-		frameArray.unshift(capture.get(0, 0, width, height));
-        image(frameArray[frameArray.length - 1], 0, 0);
-
+		frameArray.unshift(capture.get(0, 0, capture.width, capture.height));
+        
 		if (i > delay) {
-			frameArray = frameArray.slice(0, delay);
-            let startZeit2 = new Date()
-            console.log(startZeit2 - startZeit);
+            frameArray = frameArray.slice(0, delay);
+            image(frameArray[frameArray.length - 1], 0, 0);
+            i = delay
 		}else{
+            background(200);
+
+            image(frameArray[frameArray.length - 1], 0, 0);
             textSize(40);
             fill(255)
-            text("lädt...", width/2-50, height/2)
+            text("delay...", width/2-50, height/2)
         }
 	}
 	i++;
